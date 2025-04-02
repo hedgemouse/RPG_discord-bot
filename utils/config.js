05e7a07ffb4389687129342598a77b6346370e09
hedgemouse <lenhardtnimrod@gmail.com>
2025-04-02 20:02:36 +0200

    dotenv használata

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
     bot: {
-        token: '',
-        guild: '',
-        id: ''
+        token: process.env.TOKEN,
+        guild: process.env.GUILD,
+        id: process.env.BOT_ID
     }
-}
+};
